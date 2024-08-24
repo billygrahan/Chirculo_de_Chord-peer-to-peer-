53005a5d8bf7c9094b18b9e1821f2eaeaa9b5248
JVictor011 <jvictor2021@alu.ufc.br>
2024-08-24 20:08:34 -0300

    figer table funcionando com ids manuais

--- main.py
+++ main.py
@@ -4,9 +4,31 @@ import threading
 import readchar
 import sys
 from time import sleep
+import math
 from data_com import DataCom
 from servidor import Servidor
 from cliente import Cliente
+
+class Node:
+    def __init__(self, identifier, m):
+        self.identifier = identifier
+        self.m = m
+        self.finger_table = []
+
+    def calculate_finger_table(self, nodes):
+        for i in range(1, self.m + 1):
+            start = (self.identifier + 2**(i-1)) % 2**self.m
+            successor = self.find_successor(start, nodes)
+            self.finger_table.append(successor)
+
+    def find_successor(self, id, nodes):
+        for node in nodes:
+            if node.identifier >= id:
+                return node.identifier
+        return nodes[0].identifier
+
+    def __repr__(self):
+        return f"Node({self.identifier}): {self.finger_table}"
 
 def main():
     numero_de_pares = 2
@@ -50,6 +72,19 @@ def main():
             cliente.close()
         except Exception as e:
             print("Erro ao fechar o cliente: ", str(e))
+        
+    try:
+        node_ids = [0, 1, 3, 4, 6]
+        m = math.ceil(math.log2(len(node_ids)))
+        nodes = [Node(id, m) for id in node_ids]
+
+        for node in nodes:
+            node.calculate_finger_table(nodes)
+
+        for node in nodes:
+            print(node)
+    except Exception as e:
+        print("Erro: ", str(e))
 
 
 if __name__ == "__main__":
